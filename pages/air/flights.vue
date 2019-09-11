d48a7d2a79e729f8ae3368e62c21868a9656ce9f
初心者f <1061707176@qq.com>
2019-09-11 16:47:53 +0800

    完成了筛选框基本布局

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,60 +1,109 @@
 <template>
-    <section class="contianer">
-        <el-row  type="flex" justify="space-between">
+  <section class="contianer">
+    <el-row type="flex" justify="space-between">
+      <!-- 顶部过滤列表 -->
+      <div class="flights-content">
+        <!-- 过滤条件 -->
+        <div>
+          <section >
+            <!-- 其他代码... -->
 
-            <!-- 顶部过滤列表 -->
-            <div class="flights-content">
-                <!-- 过滤条件 -->
-                <div>
-                    
-                </div>
-                
-                <!-- 航班头部布局 -->
-                <div>
-                    <flightsListHead />
-                </div>
-                
-                
-                <!-- 航班信息 -->
-                <div>
-                    <flightsItem v-for="(item,index) in airlist" :key="index" :data='item'/>
-                </div>
-            </div>
+            <!-- 过滤条件 -->
+            <!-- data 是不会被修改的列表数据 -->
+            <!-- setDataList 用于修改过滤后的数组列表 -->
+            <FlightsFilters :data="cacheFlightsData" />
 
-            <!-- 侧边栏 -->
-            <div class="aside">
-                <!-- 侧边栏组件 -->
-            </div>
-        </el-row>
-    </section>
+            <!-- 其他代码... -->
+          </section>
+        </div>
+
+        <!-- 航班头部布局 -->
+        <div>
+          <flightsListHead />
+        </div>
+
+        <!-- 航班信息 -->
+        <div>
+          <flightsItem v-for="(item,index) in dataList" :key="index" :data="item" />
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="pageIndex"
+            :page-sizes="[5, 10, 15, 20]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="total"
+          ></el-pagination>
+        </div>
+      </div>
+
+      <!-- 侧边栏 -->
+      <div class="aside">
+        <!-- 侧边栏组件 -->
+      </div>
+    </el-row>
+  </section>
 </template>
 
 <script>
-
 import moment from "moment";
-import flightsListHead from '@/components/air/flightsListHead'
-import flightsItem from '@/components/air/flightsItem'
+import flightsListHead from "@/components/air/flightsListHead";
+import flightsItem from "@/components/air/flightsItem";
+import FlightsFilters from '@/components/air/flightsFilters'
 
 export default {
-    data(){
-        return {
-            airlist:[]
-        }
-    },components:{
-        flightsListHead,flightsItem
+  data() {
+    return {
+      airlist: [],
+      flightsData: {},
+      // 当前显示的列表数组
+      dataList: [],
+    cacheFlightsData:{
+      info:{},
+      options:{},
+      flights:{}
     },
-    mounted(){
-        this.$axios({
-           url: "airs",
-           params: this.$route.query
-        }).then(res=>{
-            console.log(res)
-            this.airlist=res.data.flights
-        })
+      pageIndex: 1, // 当前的页码
+      pageSize: 5, // 当前的条数
+      total: 0 // 总条数
+    };
+  },
+  components: {
+    flightsListHead,
+    flightsItem,
+    FlightsFilters
+  },
+  mounted() {
+    this.$axios({
+      url: "airs",
+      params: this.$route.query
+    }).then(res => {
+      console.log(res);
+      this.airlist = res.data.flights;
+      this.total = this.airlist.length;
+        this.cacheFlightsData={...res.data}
+      this.dataList = this.airlist.slice(0, this.pageSize);
+      console.log(this.cacheFlightsData)
+      
+    });
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(val);
+      this.pageSize = val;
+      this.dataList = this.airlist.slice(0, this.pageSize);
+    },
+    handleCurrentChange(val) {
+      console.log(val);
+      this.pageIndex = val;
+      this.dataList = this.airlist.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
     }
-}
+  }
+};
 </script>
-
 <style scoped lang="less">
     .contianer{
         width:1000px;
@@ -69,4 +118,4 @@ export default {
     .aside{
         width:240px;
     } 
-</style> 
+</style>
